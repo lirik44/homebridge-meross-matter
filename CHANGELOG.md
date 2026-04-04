cfc4f86c5c9b0d6077d777eea3ad4fb27a88ecab
Ben <43026681+bwp91@users.noreply.github.com>
2026-04-04 18:58:16 +0100

    dependency updates + code lint fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to `@homebridge-plugins/homebridge-meross` will be documented in this file.
 
+## v10.12.1 (Pending Release)
+
+### Changed
+
+- dependency updates + code lint fixes
+
 ## v10.12.0 (2026-02-28)
 
 ### Added
--- eslint.config.js
+++ eslint.config.js
@@ -36,9 +36,9 @@ export default antfu(
       'style/quote-props': ['error', 'consistent-as-needed'],
       'test/no-only-tests': 'error',
       'unicorn/no-useless-spread': 'error',
-      'unused-imports/no-unused-vars': 0,
+      'unused-imports/no-unused-vars': ['error', { caughtErrors: 'none' }],
     },
-    typescript: true,
+    typescript: false,
   },
   {
     files: ['**/*.md'],
